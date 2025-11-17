db9b3b437e2fd8d7cbe5a3867470e7617fc7da9a
Антон Костин <xxxtonchxxx@gmail.com>
2025-11-17 21:27:48 +0300

    Поправил склонения в админке

--- backend/apps/management/apps.py
+++ backend/apps/management/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class ManagementConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
+    verbose_name = "Управление командами"
     name = "apps.management"
--- backend/apps/management/models.py
+++ backend/apps/management/models.py
@@ -1,6 +1,10 @@
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils import timezone
+
+
+def current_year() -> int:
+    return timezone.now().year
 
 
 class Faculty(models.Model):
@@ -14,26 +18,22 @@ class Faculty(models.Model):
         updated_at (datetime): Дата и время последнего обновления записи
     """
 
-    name = models.CharField(max_length=255, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
 
     class Meta:
         ordering = ["name"]
-        verbose_name = "Факультет"
+        verbose_name = "факультет"
         verbose_name_plural = "Факультеты"
 
     def __str__(self) -> str:
         return str(self.name)
 
 
-def current_year() -> int:
-    return int(timezone.now().year)
-
-
 class Group(models.Model):
     """
-    Модель группы
+    Модель студенческой группы
 
     Attributes:
         id (int): (Unique) первичный ключ
@@ -44,21 +44,23 @@ class Group(models.Model):
         updated_at (datetime): Дата и время последнего обновления записи
     """
 
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name="Название")
     faculty = models.ForeignKey(
-        Faculty, on_delete=models.CASCADE, related_name="groups"
+        Faculty,
+        on_delete=models.CASCADE,
+        related_name="groups",
+        verbose_name="Факультет",
     )
     year = models.IntegerField(
-        default=current_year, validators=[MinValueValidator(2000)]
+        default=current_year, validators=[MinValueValidator(2000)], verbose_name="Год"
     )
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
 
     class Meta:
-        ordering = ["faculty__name", "name", "year"]
+        ordering = ["faculty__name", "year", "name"]
         unique_together = ["name", "faculty", "year"]
-        verbose_name = "Студенческая группа"
+        verbose_name = "студенческую группу"
         verbose_name_plural = "Студенческие группы"
 
     def __str__(self) -> str:
@@ -77,15 +79,17 @@ class Team(models.Model):
         updated_at (datetime): Дата и время последнего обновления записи
     """
 
-    name = models.CharField(max_length=255)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="teams")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, verbose_name="Название")
+    group = models.ForeignKey(
+        Group, on_delete=models.CASCADE, related_name="teams", verbose_name="Группа"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
 
     class Meta:
-        ordering = ["group__faculty__name", "group__name", "name"]
+        ordering = ["group__faculty__name", "group__year", "group__name", "name"]
         unique_together = ["name", "group"]
-        verbose_name = "Команда"
+        verbose_name = "команду"
         verbose_name_plural = "Команды"
 
     def __str__(self) -> str:
